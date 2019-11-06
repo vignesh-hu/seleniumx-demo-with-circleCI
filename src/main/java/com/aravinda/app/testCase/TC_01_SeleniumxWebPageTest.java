b366ab4bb669524db54d62ede046097c065cb39a
Aravinda Weerasekara <aravindaweerasekara@gmail.com>
2019-11-06 20:17:09 +0100

    Update test case to run in docker image

--- src/main/java/com/aravinda/app/testCase/TC_01_SeleniumxWebPageTest.java
+++ src/main/java/com/aravinda/app/testCase/TC_01_SeleniumxWebPageTest.java
@@ -9,13 +9,21 @@ import org.seleniumx.util.TestCase;
 
 public class TC_01_SeleniumxWebPageTest extends TestCase {
 
-    @ServerSettings(PLATFORM = Platform.MAC,
+    @ServerSettings(
+            PLATFORM = Platform.LINUX,
             BROWSER = Set.BROWSER.CHROME,
             VERSION = "1.0",
             SERVER_PORT = "4444",
             SERVER_URL = "http://localhost",
-            BASE_URL = "http://localhost:8080/address-formatter-0.0.1-SNAPSHOT",
-            IMPLICIT_WAIT = 10)
+            BASE_URL = "https://www.seleniumx.com/",
+            IMPLICIT_WAIT = 100)
+//    @DriverSettings(
+//            OS = Set.OS.MAC,
+//            WINDOW_SIZE = Set.WINDOW_SIZE.DEFAULT,
+//            BROWSER = Set.BROWSER.CHROME,
+//            BASE_URL = "https://www.seleniumx.com/",
+//            IMPLICIT_WAIT = 100
+//    )
     @Script(script = SeleniumxWebPageTestScript.class)
     public void testCase() {
         run();
