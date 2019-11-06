f4aea391152115a5c92962b766f9e5cb90b300a0
Aravinda Weerasekara <aravindaweerasekara@gmail.com>
2019-11-06 18:25:18 +0100

    update circleCI yml and TC1 Test case

--- src/main/java/com/aravinda/app/testCase/TC1_SeleniumxWebPageTest.java
+++ src/main/java/com/aravinda/app/testCase/TC1_SeleniumxWebPageTest.java
@@ -1,27 +1,28 @@
 package com.aravinda.app.testCase;
 
 import com.aravinda.app.script.SeleniumxWebPageTestScript;
-import org.seleniumx.annotations.DriverSettings;
+import org.openqa.selenium.Platform;
 import org.seleniumx.annotations.Script;
+import org.seleniumx.annotations.ServerSettings;
 import org.seleniumx.util.Set;
 import org.seleniumx.util.TestCase;
 
 public class TC1_SeleniumxWebPageTest extends TestCase {
 
-    //    @ServerSettings(PLATFORM = Platform.MAC,
-//            BROWSER = Set.BROWSER.CHROME,
-//            VERSION = "1.0",
-//            SERVER_PORT = "4444",
-//            SERVER_URL = "http://localhost",
-//            BASE_URL = "http://localhost:8080/address-formatter-0.0.1-SNAPSHOT",
-//            IMPLICIT_WAIT = 10)
-    @DriverSettings(
-            OS = Set.OS.MAC,
-            WINDOW_SIZE = Set.WINDOW_SIZE.DEFAULT,
+    @ServerSettings(PLATFORM = Platform.MAC,
             BROWSER = Set.BROWSER.CHROME,
-            BASE_URL = "https://www.seleniumx.com/",
-            IMPLICIT_WAIT = 10000
-    )
+            VERSION = "1.0",
+            SERVER_PORT = "4444",
+            SERVER_URL = "http://localhost",
+            BASE_URL = "http://localhost:8080/address-formatter-0.0.1-SNAPSHOT",
+            IMPLICIT_WAIT = 10)
+//    @DriverSettings(
+//            OS = Set.OS.MAC,
+//            WINDOW_SIZE = Set.WINDOW_SIZE.DEFAULT,
+//            BROWSER = Set.BROWSER.CHROME,
+//            BASE_URL = "https://www.seleniumx.com/",
+//            IMPLICIT_WAIT = 10000
+//    )
     @Script(script = SeleniumxWebPageTestScript.class)
     public void testCase() {
         run();
